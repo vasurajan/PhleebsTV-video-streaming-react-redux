2a16c55b6220737413ba653fad05e224cd8989a7
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-14 22:53:06 +0530

    Adding header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-     App
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/new" component={StreamCreate} />
+          <Route exact path="/edit" component={StreamEdit} />
+          <Route exact path="/delete" component={StreamDelete} />
+          <Route exact path="/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
