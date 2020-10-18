434ec0c90de4de4e3b37ce5edc62caa249f365a2
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-18 17:01:45 +0530

    associating users with id

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,14 @@
 import streams from '../apis/streams'
 
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  SIGN_IN,
+  SIGN_OUT
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -17,6 +25,58 @@ export const signOut = () => {
 
 
 // action creator for handling creation of streams
-export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues)
+export const createStream = formValues => async (dispatch, getState) => {
+  const {userId} = getState().auth;
+  const response = await streams.post('/streams', {...formValues, userId});
+
+  dispatch({
+    type: CREATE_STREAM,
+    payload: response.data
+  })
+}
+
+
+// action creator for handling fetching of streams
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams')
+
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data
+  })
+}
+
+
+// action creator for fetching of a stream
+export const fetchStream = (id) => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data
+  })
+}
+
+
+
+// action creator for handling editing/updating a stream
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues)
+
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data
+  })
+}
+
+
+
+// action creator for handling deletion of a stream
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`)  // no response since we only need to delete stream with particular id
+
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id
+  })
 }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer // giving a more descriptive name to our reducer from redux-form
+  form: formReducer, // giving a more descriptive name to our reducer from redux-form
+  streams: streamReducer
 });
