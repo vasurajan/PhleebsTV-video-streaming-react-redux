d09739fa2942d38237ec40ee0111a013471af559
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-18 23:40:54 +0530

    rtmp server with config installed and running on 8000

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -16,11 +16,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route exact path="/" component={StreamList} />
-          <Route exact path="/streams/new" component={StreamCreate} />
-          <Route exact path="/streams/edit/:id" component={StreamEdit} />
-          <Route exact path="/streams/delete/:id" component={StreamDelete} />
-          <Route exact path="/streams/:id" component={StreamShow} />
+          <Switch>
+            <Route exact path="/" component={StreamList} />
+            <Route exact path="/streams/new" component={StreamCreate} />
+            <Route exact path="/streams/edit/:id" component={StreamEdit} />
+            <Route exact path="/streams/delete/:id" component={StreamDelete} />
+            <Route exact path="/streams/:id" component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
